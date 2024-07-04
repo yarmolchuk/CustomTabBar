a0b69a6738ec3caed80dec4d4b172df8f5fbfb59
Dmitriy Yarmolchuk <dmitriy.yarmolchuk@fintech-farm.com>
2024-07-04 11:47:56 +0300

    Custom TabBar SwiftUI

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Dmytro Yarmolchuk on 04.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
